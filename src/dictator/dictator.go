ea834f875f0aa4dfc6340709e61793dbeb0f7343
Maxime Fouilleul <maxime.fouilleul@blablacar.com>
2016-05-09 14:40:10 +0200

    Add status/ping command to http listner

--- src/dictator/dictator.go
+++ src/dictator/dictator.go
@@ -3,14 +3,28 @@ package main
 import (
 	log "github.com/Sirupsen/logrus"
 	"strconv"
+	"fmt"
 	"net/http"
 )
+
+
+func HTTPDictatorStatus(w http.ResponseWriter, r *http.Request, ze *Elector) {
+	if ze.Paused {
+		fmt.Fprintf(w, "Dictator Status = Disable/Paused\n")
+		log.Info("Dictator Status = Disable/Paused")
+	}else{
+		fmt.Fprintf(w, "Dictator Status = Enable\n")
+		log.Info("Dictator Status = Enable")
+	}	
+}
+
 
 func HTTPEnable(w http.ResponseWriter, r *http.Request, ze *Elector) {
 	if ze.Paused {
 		go ze.Run()
 		ze.Paused = false
 		log.Info("Unpause Dictator")
+		fmt.Fprintf(w, "Dictator Status = Enable\n")
 	}else{
 		log.Warn("Dictator is already enabled")
 	}
@@ -21,6 +35,7 @@ func HTTPDisable(w http.ResponseWriter, r *http.Request, ze *Elector) {
 		ze.Destroy()
 		ze.Paused = true
 		log.Info("Pause Dictator")
+		fmt.Fprintf(w, "Dictator Status = Disable/Paused\n")
 	}else{
 		log.Warn("Dictator is already paused")
 	}
@@ -45,6 +60,12 @@ func Run(conf DictatorConfiguration, stop <-chan bool, finished chan<-bool) {
     go ze.Run()
 
     // http signals management
+    http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+          HTTPDictatorStatus(w, r, &ze)
+    })
+    http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+          HTTPDictatorStatus(w, r, &ze)
+    })
     http.HandleFunc("/enable", func(w http.ResponseWriter, r *http.Request) {
           HTTPEnable(w, r, &ze)
     })
